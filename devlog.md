c1a7d70dc9b90b7c6ab86c37cdb714f0803c70c4
Jacob Cunningham <jcun4128@gmail.com>
2023-07-01 05:41:14 -0500

    want basic slider

---
+++ devlog.md
@@ -0,0 +1,25 @@
+07/01/2023
+
+5:23 AM
+
+checking why this photo viewer is not in use
+
+5:25 AM
+
+this app doesn't work lol, at least no errors, just a blank page
+
+5:27 AM
+
+wow I worked on this 3 years ago, also my plants are massive now, like my apple trees one of them is 6 feet tall though grown wrong (just tall and skinny)
+
+5:34 AM
+
+okay... I got it able to render but need to make the actual photo scan/load/sliding bit
+
+Ahh... I've built so many photo sliders already ha
+
+it would be cool to auto-slide through the photos
+
+5:40 AM
+
+I can't write this code now, I've been up more than 12 hours at this point
--- web-interface/photo-viewer/src/App.js
+++ web-interface/photo-viewer/src/App.js
@@ -3,27 +3,23 @@ import './App.css';
 import { getTodaysDate } from './utils/date';
 
 const App = () => {
-
   const loadImages = () => {
     let todaysDate = getTodaysDate();
-    const curUrl = window.location.href;
-    let photosBasePath = '/var/www/html/plant-photos/';
-    let baseDate = '2020-07-24'; // for demo/dev purposes
-
-    if (curUrl.indexOf('localhost') !== -1) {
-      photosBasePath = curUrl + 'plant-photos-dev/';
-      todaysDate = baseDate;
-    }
+    let photosBasePath = 'http://192.168.1.115/plant-photos/';
 
     const img = new Image();
-    img.src = photosBasePath + todaysDate + '.jpg'; console.log(photosBasePath + todaysDate + '.jpg');
+    img.src = photosBasePath + '2023-06-23' + '.jpg'; console.log(photosBasePath + todaysDate + '.jpg');
     img.onload = () => { console.log('loaded') };
     img.onerror = () => { console.log('failed') };
+
+    return <img src={img.src}/>;
   }
 
   return (
     <div className="App">
-      {loadImages()}
+      <div className="App__slider">
+        {loadImages()}
+      </div>
     </div>
   );
 }
